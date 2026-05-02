42b22a4ff977616f84997d0ffee0bc5c3a985a9c
Chad Smith <clsmith5@student.fullsail.edu>
2026-05-02 12:38:17 -0500

    Increased rate limit in proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -12,7 +12,7 @@ function getRatelimiter() {
 
   return new Ratelimit({
     redis: new Redis({ url, token }),
-    limiter: Ratelimit.slidingWindow(10, "30 d"),
+    limiter: Ratelimit.slidingWindow(30, "30 d"),
     analytics: false,
     prefix: "lsa:audit-v2",
   });
