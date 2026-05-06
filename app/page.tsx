dd50b2cbcbc1008c8963e48b0c6f9827a215bdad
Chad Smith <clsmith5@student.fullsail.edu>
2026-05-06 08:30:11 -0500

    started redesign

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import SiteNavMinimal from "@/app/components/SiteNavMinimal";
 import HeroSection from "@/app/components/HeroSection";
-import TrustBar from "@/app/components/TrustBar";
-import HowItWorksSection from "@/app/components/HowItWorksSection";
-import WhatWeCheckSection from "@/app/components/WhatWeCheckSection";
-import TestimonialsSection from "@/app/components/TestimonialsSection";
-import FaqSection from "@/app/components/FaqSection";
-import FinalCtaSection from "@/app/components/FinalCtaSection";
 import SiteFooterMinimal from "@/app/components/SiteFooterMinimal";
 import styles from "@/styles/landing.module.css";
 
@@ -15,12 +9,6 @@ export default function Home() {
       <SiteNavMinimal />
       <main className={styles.mainContent}>
         <HeroSection />
-        <TrustBar />
-        <HowItWorksSection />
-        <WhatWeCheckSection />
-        <TestimonialsSection />
-        <FaqSection />
-        <FinalCtaSection />
       </main>
       <SiteFooterMinimal />
     </>
